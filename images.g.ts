13e803432b4d012b1633c9fbb568144f625106c2
1093307-landon <1093307@stu.tylerisd.org>
2026-02-06 19:43:07 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "retro":return hex`0078000408020202001c000c960064006d019001000478002c010000640032000000000a0600051b0004000800011b08000c00031b1e220c001000012518001c0002222908001c000e050046006603320000040a002d00000064001400013200020100021f001400180001222000280002222928003000011d300038000127380040000120`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,22 @@
+music.play(music.createSong(assets.song`retro`), music.PlaybackMode.UntilDone)
+forever(function () {
+    controller.moveSprite(sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player))
+    game.setGameOverPlayable(true, music.melodyPlayable(music.powerUp), true)
+})
